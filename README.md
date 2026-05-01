2f0343c19bdd7786a36ee0d59b2b2f9209d044c5
Sajanth Kirubaharan <sajanth02@gmail.com>
2026-05-01 15:58:25 +0100

    finished roll dice app

--- README.md
+++ README.md
@@ -1,9 +1,35 @@
-## Roll a Dice Game 
+# Roll a Dice Game 
 
-** A simple JavaScript game where players role a dice. Players accrue points each round and 30 points are required to win the game.nts first to win the game**
+** A simple JavaScript game where players role a dice. Players accrue points each round and 30 points are required to win the game**
 
-#Features
-1. Players can change names
-2. Players can be added to the game
-3. Players click a button to roll the dice. Each player has 1 dice.
-4. A scoreboard will be available for all players to view. Players are added to the scoreboard and the points earned refresh each round. 
+## Features
+1. Players name customisation:
+    - Players can customise their names when they join the game.
+2. Add multiple players:
+    - New players can be added to the game and their name will be added to the scoreboard.
+3. Dice roll button:
+    - Each player has their own dice. Clicking the dice will generate a random number and add it to the score.
+4. Real time scoreboard:
+    - The scoreboard updates dynamically after each round to reflect the current points of all the players.
+5. Edit name:
+    - Players can edit their name at any time by clicking the "Edit Names" button.
+6. Reset Score:
+    - Players can reset the game scores to zero by clicking the "Reset Score" button. 
+
+## How to start the game
+1. Clone repo
+2. Open index.html in your favourite browser
+3. To reset game refresh page
+
+## How to Play
+1. Add Players:
+    - Players can enter their name and click the "Edit Names" button to join the game.
+2. Roll the Dice:
+    - Each player rolls the dice. A random number between 1 and 6 will be generated for each roll, and the points are added to the player's total score for that round. 
+3. Scoreboard:
+    - The game features a live scoreboard that shows all players and their current points. 
+4. Win Condition:
+    - The first player to reach 30 points wins the game. When a player wins, the game will notify them.
+
+
+
--- game.js
+++ game.js
@@ -5,6 +5,9 @@ let player1 = "Player 1";
 let player2 = "Player 2";
 let player3 = "Player 3";
 let score = {};
+score[player1] = 0;
+score[player2] = 0;
+score[player3] = 0;
 
 // Function allowing users to change their names
 function editNames() {
@@ -12,7 +15,7 @@ function editNames() {
   player2 = prompt("Change name for Player Two");
   player3 = prompt("Change name for Player Three");
 
-// Assigning the new names
+  // Assigning the new names
   document.querySelector("p.PlayerOne").innerHTML = player1;
   document.querySelector("p.PlayerTwo").innerHTML = player2;
   document.querySelector("p.PlayerThree").innerHTML = player3;
@@ -22,9 +25,8 @@ function editNames() {
   updateScoreboard();
 }
 
-// Initialise scores for players
-
-
+const editNameBtn = document.querySelector("#editName");
+editNameBtn.addEventListener("click", editNames);
 
 const btnOne = document.querySelector("#btnOne");
 const btnTwo = document.querySelector("#btnTwo");
@@ -38,23 +40,28 @@ function rollTheDice(playerName) {
   const randomNumber = Math.floor(Math.random() * 6) + 1;
   console.log(randomNumber);
   score[playerName] += randomNumber;
-  updateScoreboard(); // add a function to update scoreboard
+
+  if (playerName === player1) {
+    document.querySelector("#img1").src = `./dice/dice${randomNumber}.png`;
+  }
+  if (playerName === player2) {
+    document.querySelector("#img2").src = `./dice/dice${randomNumber}.png`;
+  }
+  if (playerName === player3) {
+    document.querySelector("#img3").src = `./dice/dice${randomNumber}.png`;
+  }
+
+  updateScoreboard(); // Add a function to update scoreboard
   if (score[playerName] >= 30) {
-    prompt(`Congrats ${playerName}, you have won!!!`);
+    alert(`Congrats ${playerName}, you have won !!!`);
     return;
   }
 }
 
-// const scoreBoard = document.querySelector("#scoreBoard");
-// const scoreCard = document.querySelectorAll("#scoreCard");
-// const scoreName = document.querySelectorAll(".scoreName");
-
 function updateScoreboard() {
-  document.querySelector("#scoreOne").innerHTML = score[player1];
-  document.querySelector("#scoreTwo").innerHTML = score[player2];
-  document.querySelector("#scoreThree").innerHTML = score[player3];
-
-  //   document.querySelector("#[playerName").innerText = score[playerName];
+  document.querySelector("#scoreOne").innerHTML = `Score : ${score[player1]}`;
+  document.querySelector("#scoreTwo").innerHTML = `Score : ${score[player2]}`;
+  document.querySelector("#scoreThree").innerHTML = `Score : ${score[player3]}`;
 }
 
 // Reset the game
@@ -62,6 +69,8 @@ function resetGame() {
   score[player1] = 0;
   score[player2] = 0;
   score[player3] = 0;
+  document.querySelector("#img1").src = `./dice/dice1.png`;
+  document.querySelector("#img2").src = `./dice/dice2.png`;
+  document.querySelector("#img3").src = `./dice/dice3.png`;
   updateScoreboard();
 }
-
